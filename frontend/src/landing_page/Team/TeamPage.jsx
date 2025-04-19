49bf4d2079344993e2239976bd70c65ed40bd356
Sahil Kamble <syk2448@gmail.com>
2025-04-19 21:00:09 +0530

    team page

--- frontend/src/landing_page/Team/TeamPage.jsx
+++ frontend/src/landing_page/Team/TeamPage.jsx
@@ -6,13 +6,31 @@ import Anushka from "/MemberImages/Anushka.webp";
 import Karan from "/MemberImages/Karan.webp";
 import Ram from "/MemberImages/Ram.webp";
 // import Saksham from "/MemberImages/Saksham.webp";
-import Sameer from "/MemberImages/Sameer.webp";
+import Sameer from "/MemberImages/Sameer.png";
 import Shaurya from "/MemberImages/Shaurya.webp";
 import Saumya from "/MemberImages/Saumya.webp";
 import V_Anuj from "/MemberImages/V_Anuj.webp";
 import Vidish from "/MemberImages/Vidish.webp";
-import Yashasvi from "/MemberImages/Yashasvi.webp";
+import Yashaswi from "/MemberImages/Yashaswi.webp";
 import sahil from "/MemberImages/sahil.webp";
+import Pratham from "/MemberImages/Pratham.webp";
+import Promit from "/MemberImages/Promit.webp";
+import Prachi from "/MemberImages/Prachi.webp";
+import Naresh from "/MemberImages/Naresh.webp";
+import Kore from "/MemberImages/Kore.webp";
+import Mehul from "/MemberImages/Mehul.webp";
+import Aryan from "/MemberImages/Aryan.webp";
+import Argh from "/MemberImages/Argh.webp";
+import Shagun from "/MemberImages/Shagun.webp";
+import Ved from "/MemberImages/Ved.webp";
+import Aastha from "/MemberImages/Aastha.webp";
+import Shyam from "/MemberImages/Shyam.webp";
+import Rishika from "/MemberImages/Rishika.webp";
+import Navin from "/MemberImages/Navin.webp";
+import ShauryaSinghal from "/MemberImages/ShauryaSinghal.webp";
+import Lakshya from "/MemberImages/Lakshya.webp";
+import KumarAnuj from "/MemberImages/KumarAnuj.webp";
+
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef } from "react";
 // import gsap from "gsap";
@@ -20,7 +38,14 @@ import { useRef } from "react";
 function TeamPage() {
   const teamRef = useRef(null);
 
-  const Advisors = [
+  const Advisory = [
+    {
+      name: "Lakshya ",
+      role: "Advisor",
+      image: `${Lakshya}`,
+    },
+  ];
+  const Convenors = [
     {
       name: "Saumya Singh",
       role: "Curator",
@@ -32,16 +57,33 @@ function TeamPage() {
       image: `${Vidish}`,
     },
   ];
+
   const teamMembers = [
     {
       name: "Anukriti Sethi",
-      role: "Advisory",
+      role: "Organizing Committee",
       image: `${Anukriti}`,
     },
     {
       name: "Yashashvi Chaudhary",
-      role: "Advisory",
-      image: `${Yashasvi}`,
+      role: "Organizing Committee",
+      image: `${Yashaswi}`,
+    },
+
+    {
+      name: "Shyam",
+      role: "Organizing Committee",
+      image: `${Shyam}`,
+    },
+    {
+      name: "Rishika",
+      role: "Organizing Committee",
+      image: `${Rishika}`,
+    },
+    {
+      name: "Navin",
+      role: "Organizing Committee",
+      image: `${Navin}`,
     },
 
     {
@@ -59,11 +101,51 @@ function TeamPage() {
       role: "Organizing Committee",
       image: `${Shaurya}`,
     },
+    {
+      name: "Pratham Garg",
+      role: "Senior Manager",
+      image: `${Pratham}`,
+    },
+    {
+      name: "Shagun Agrawaal",
+      role: "Senior Manager",
+      image: `${Shagun}`,
+    },
+    {
+      name: "Prachi Sawant",
+      role: "Senior Manager",
+      image: `${Prachi}`,
+    },
+    {
+      name: "Aryan",
+      role: "Senior Manager",
+      image: `${Aryan}`,
+    },
+
     // {
     //   name: "Saksham Jain",
     //   role: "Organizing Committee",
     //   image: `${Saksham}`,
     // },
+
+    {
+      name: "Argh",
+      role: "Senior Manager",
+      image: `${Argh}`,
+    },
+
+    {
+      name: "Sahil Kamble",
+      role: "Senior Manager",
+      image: `${sahil}`,
+    },
+
+    {
+      name: "V.Anuj",
+      role: "Senior Manager",
+      image: `${V_Anuj}`,
+    },
+
     {
       name: "Sameer Modi",
       role: "Senior Manager",
@@ -75,36 +157,74 @@ function TeamPage() {
       image: `${Ram}`,
     },
     {
-      name: "Sahil Kamble",
+      name: "Naresh Rajput",
       role: "Senior Manager",
-      image: `${sahil}`,
+      image: `${Naresh}`,
     },
     {
-      name: "V.Anuj",
+      name: "Shaurya",
       role: "Senior Manager",
-      image: `${V_Anuj}`,
-    },
-    {
-      name: "Anushka Tyagi",
-      role: "Manager",
-      image: `${Anushka}`,
+      image: `${ShauryaSinghal}`,
     },
 
     {
-      name: "Anuj Kumar",
+      name: "Kore",
+      role: "Senior Manager",
+      image: `${Kore}`,
+    },
+    {
+      name: "Ved",
       role: "Manager",
-      image: `${Anuj}`,
+      image: `${Ved}`,
+    },
+    {
+      name: "Anuj",
+      role: "Manager",
+      image: `${KumarAnuj}`,
+    },
+    {
+      name: "Mehul",
+      role: "Manager",
+      image: `${Mehul}`,
+    },
+    {
+      name: "Aastha",
+      role: "Manager",
+      image: `${Aastha}`,
+    },
+    {
+      name: "Promit",
+      role: "Manager",
+      image: `${Promit}`,
     },
   ];
 
   return (
     <section
       className="py-16 px-6 w-full min-h-screen"
-      style={{ backgroundColor: "#0E0E0E" }}
+      style={{ backgroundColor: "black" }}
     >
       <h1 className="text-center text-[#555555] text-5xl md:text-6xl font-extrabold mb-16">
         MEET OUR TEAM
       </h1>
+      <div className="flex flex-col gap-14">
+        <h1 className="text-center text-[white] text-3xl md:text-4xl font-extrabold -tracking-tighter">
+          Advisory
+        </h1>
+        <div
+          className="flex flex-wrap justify-between px-2 gap-0 xl:px-50 items-center"
+          ref={teamRef}
+        >
+          {Advisory.map((member, index) => (
+            <TeamMemberCard
+              key={index}
+              name={member.name}
+              role={member.role}
+              image={member.image}
+            />
+          ))}
+        </div>
+      </div>
       <div className="flex flex-col gap-14">
         <h1 className="text-center text-[white] text-3xl md:text-4xl font-extrabold -tracking-tighter">
           Convenors
@@ -113,7 +233,7 @@ function TeamPage() {
           className="flex flex-wrap justify-between px-2 gap-0 xl:px-50 items-center"
           ref={teamRef}
         >
-          {Advisors.map((member, index) => (
+          {Convenors.map((member, index) => (
             <TeamMemberCard
               key={index}
               name={member.name}
@@ -124,9 +244,12 @@ function TeamPage() {
         </div>
       </div>
 
-      <div>
+      <div className="flex flex-col gap-14">
+        <h1 className="text-center text-[white] text-3xl md:text-4xl font-extrabold -tracking-tighter">
+          Team
+        </h1>
         <div
-          className="flex flex-wrap px-2 sm:px-0 mx-auto justify-between items-center"
+          className="flex flex-wrap px-2 sm:px-0  lg:gap-12 mx-auto justify-between items-center"
           ref={teamRef}
         >
           {teamMembers.map((member, index) => (
